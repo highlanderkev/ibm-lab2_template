ce6c36079622a49d09b6300484173b0b307af130
Kevin Westropp <highlanderkev@proton.me>
2023-11-10 20:59:13 -0500

    init commit

---
+++ settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'OTM1My1rZXZpbndl',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'crud',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
